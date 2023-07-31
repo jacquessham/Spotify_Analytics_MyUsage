71b3968c06f004ada63d08bb2b717b59dabbddbe
Jacques Sham <jacquessham2015@gmail.com>
2023-07-31 12:58:57 -0700

    Add reset DAG for testing

---
+++ Setup/dags/reset.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+default_args = {
+	'owner': 'Jacques Sham',
+	'retries': 5,
+	'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+	default_args=default_args,
+	dag_id='dev_reset_v1',
+	start_date=datetime(2023,7,15),
+	schedule_interval='0 2 * * Mon'
+	) as dag:
+	task1 = PostgresOperator(
+		task_id='reset_tables',
+		postgres_conn_id='postgres_airflow_docker_spotify',
+		sql='reset/reset.sql'
+		)
+	task1
---
+++ Setup/dags/reset/reset.sql
@@ -0,0 +1,11 @@
+-- Source Schema
+delete from src__data.src__streaming_history;
+delete from src__data.src__upload_log;
+
+-- Staging Schema
+delete from stg__data.stg__streaming_history__unique;
+
+-- Storing Schema
+delete from ctr__data.ctr__streaming_history;
+delete from ctr__data.ctr__sql_streaming_history_record_type;
+delete from ctr__data.ctr__json_streaming_history_record_type;
