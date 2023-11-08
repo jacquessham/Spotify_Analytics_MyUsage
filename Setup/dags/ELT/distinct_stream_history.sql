fad89da61a6fee50ff5305ea78522864c964b152
Jacques Sham <jacquessham2015@gmail.com>
2023-11-07 16:13:33 -0800

    Add ms_played into row_id

--- Setup/dags/ELT/distinct_stream_history.sql
+++ Setup/dags/ELT/distinct_stream_history.sql
@@ -7,13 +7,14 @@ with raw_form as (
 		episode_show_name, spotify_episode_uri, reason_start, reason_end, shuffle,
 		skipped, offline, offline_timestamp, incognito_mode, record_type,
 		row_number() over(partition by username, date_trunc('minute',ts::timestamp),
-			 master_metadata_track_name order by ms_played desc, offline_timestamp desc)
+			 master_metadata_track_name, ms_played order by offline_timestamp desc)
 			as row_id_rank
 	from src__data.src__streaming_history
 ),
 unique_history as (
 	select distinct
-		concat(username,ts_trunc,master_metadata_track_name,'_',record_type) as row_id,
+		concat(username,ts_trunc,master_metadata_track_name, ms_played,
+			'_',record_type) as row_id,
 		ts_trunc, username, platform, ms_played,
 		conn_country, ip_addr_decrypted, user_agent_decrypted,
 		master_metadata_track_name, master_metadata_album_artist_name,
